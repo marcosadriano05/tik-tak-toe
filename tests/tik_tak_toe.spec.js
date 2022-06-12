385f8e7f1fed338f6dde04a1135e6711f970f0c1
Marcos Adriano <marcosadriano740@gmail.com>
2022-06-11 22:38:41 -0300

    TikTakToe init with correct values

--- tests/tik_tak_toe.spec.js
+++ tests/tik_tak_toe.spec.js
@@ -1,5 +1,49 @@
-describe("Test", () => {
-  it("Teste", () => {
-    expect(Math.sqrt(4)).toBe(2)
+class Position {
+  constructor(row, col) {
+    this.row = row
+    this.col = col
+  }
+}
+
+class Square {
+  constructor(row, col) {
+    this.position = new Position(row, col)
+    this.value = ""
+  }
+}
+
+class TikTakToe {
+  squares = []
+
+  constructor() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        this.squares.push(new Square(i, j))
+      }
+    }
+  }
+}
+
+describe("Square", () => {
+  it("should pass the position on creation and value is a empty string", () => {
+    const square = new Square(1, 2)
+
+    expect(square.position.row).toBe(1)
+    expect(square.position.col).toBe(2)
+    expect(square.value).toEqual("")
+  })
+})
+
+describe("TikTakToe", () => {
+  it("should fill all squares on creation", () => {
+    const game = new TikTakToe()
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const square = game.squares.some(square => {
+          return square.position.row === i && square.position.col === j && square.value === ""
+        })
+        expect(square).toBeTruthy()
+      }
+    }
   })
 })
