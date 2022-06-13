165c836a454bd37b2239bb6fc58cebf9d9b83628
Marcos Adriano <marcosadriano740@gmail.com>
2022-06-13 15:41:13 -0300

    refactor: tests

--- tests/tik_tak_toe.spec.js
+++ tests/tik_tak_toe.spec.js
@@ -19,8 +19,77 @@ describe("Player", () => {
 })
 
 describe("TikTakToe", () => {
+  let game
+
+  beforeEach(() => {
+    game = new TikTakToe()
+  })
+
+  const setupUnfinishedGame = () => {
+    game.setSquareValue(2)
+    game.setSquareValue(5)
+    game.setSquareValue(8)
+    game.setSquareValue(6)
+    game.setSquareValue(9)
+  }
+
+  const setupFinishedGameWithHorizontalLines = () => {
+    game.setSquareValue(2)
+    game.setSquareValue(5)
+    game.setSquareValue(3)
+    game.setSquareValue(6)
+    game.setSquareValue(1)
+  }
+
+  const setupFinishedGameWithVerticalLines = () => {
+    game.setSquareValue(1)
+    game.setSquareValue(2)
+    game.setSquareValue(4)
+    game.setSquareValue(3)
+    game.setSquareValue(7)
+  }
+
+  const setupFinishedGameWithPrincipalDiagonalLine = () => {
+    game.setSquareValue(1)
+    game.setSquareValue(2)
+    game.setSquareValue(5)
+    game.setSquareValue(3)
+    game.setSquareValue(9)
+  }
+
+  const setupFinishedGameWithSecondaryDiagonalLine = () => {
+    game.setSquareValue(3)
+    game.setSquareValue(2)
+    game.setSquareValue(5)
+    game.setSquareValue(1)
+    game.setSquareValue(7)
+  }
+
+  const setupDrawGame = () => {
+    game.setSquareValue(3)
+    game.setSquareValue(1)
+    game.setSquareValue(2)
+    game.setSquareValue(6)
+    game.setSquareValue(4)
+    game.setSquareValue(5)
+    game.setSquareValue(9)
+    game.setSquareValue(7)
+    game.setSquareValue(8)
+  }
+
+  const setupFinishedGameWithAllSquaresFilled = () => {
+    game.setSquareValue(3)
+    game.setSquareValue(1)
+    game.setSquareValue(2)
+    game.setSquareValue(6)
+    game.setSquareValue(4)
+    game.setSquareValue(5)
+    game.setSquareValue(7)
+    game.setSquareValue(9)
+    game.setSquareValue(8)
+  }
+
   it("should fill all squares on creation", () => {
-    const game = new TikTakToe()
     const squaresCreated = Array.from(game.squares.values())
     const hasEmptyString = squaresCreated.every(square => square.value === "")
 
@@ -29,14 +98,11 @@ describe("TikTakToe", () => {
   })
 
   it("should create a game with two players with diferent values and turn values", () => {
-    const game = new TikTakToe()
-
     expect(game.playerOne.value).not.toEqual(game.playerTwo.value)
     expect(game.playerOne.itsTurn).not.toBe(game.playerTwo.itsTurn)
   })
 
   it("should set value of an selected square by position", () => {
-    const game = new TikTakToe()
     game.setSquareValue(3)
     game.setSquareValue(5)
     game.setSquareValue(7)
@@ -47,7 +113,6 @@ describe("TikTakToe", () => {
   })
 
   it("should not set value of an selected square by position if it is setted before", () => {
-    const game = new TikTakToe()
     game.setSquareValue(3)
     game.setSquareValue(3)
     
@@ -55,7 +120,6 @@ describe("TikTakToe", () => {
   })
 
   it("should change the players turn when set value", () => {
-    const game = new TikTakToe()
     game.setSquareValue(3)
     
     expect(game.playerOne.itsTurn).toBe(false)
@@ -68,90 +132,52 @@ describe("TikTakToe", () => {
   })
 
   it("should check end game returns false if the game is not finished yet", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(8)
-    game.setSquareValue(6)
-    game.setSquareValue(9)
+    setupUnfinishedGame()
 
     expect(game.checkEndGame()).toBe(false)
   })
 
   it("should check end game returns true if horizontal lines have the same value", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(3)
-    game.setSquareValue(6)
-    game.setSquareValue(1)
+    setupFinishedGameWithHorizontalLines()
 
     expect(game.checkEndGame()).toBe(true)
   })
 
   it("should check end game returns true if vertical lines have the same value", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(1)
-    game.setSquareValue(2)
-    game.setSquareValue(4)
-    game.setSquareValue(3)
-    game.setSquareValue(7)
+    setupFinishedGameWithVerticalLines()
 
     expect(game.checkEndGame()).toBe(true)
   })
 
   it("should check end game returns true if principal diagonal line has the same value", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(1)
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(3)
-    game.setSquareValue(9)
+    setupFinishedGameWithPrincipalDiagonalLine()
 
     expect(game.checkEndGame()).toBe(true)
   })
 
   it("should check end game returns true if secondary diagonal line has the same value", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(1)
-    game.setSquareValue(7)
+    setupFinishedGameWithSecondaryDiagonalLine()
 
     expect(game.checkEndGame()).toBe(true)
   })
 
   it("should don't turn player if end game returns true", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(1)
-    game.setSquareValue(7)
+    setupFinishedGameWithHorizontalLines()
 
     expect(game.playerOne.itsTurn).toBe(true)
   })
 
   it("should set game status finished if end game returns true", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(1)
-    game.setSquareValue(7)
+    setupFinishedGameWithHorizontalLines()
 
     expect(game.status).toEqual("finished")
   })
 
   it("should set game status in_progress when game init", () => {
-    const game = new TikTakToe()
-
     expect(game.status).toEqual("in_progress")
   })
 
   it("should not set values if status is finished", () => {
-    const game = new TikTakToe()
     game.status = "finished"
     game.setSquareValue(7)
 
@@ -159,18 +185,12 @@ describe("TikTakToe", () => {
   })
 
   it("should set winner equals true to current player if he's win", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(2)
-    game.setSquareValue(5)
-    game.setSquareValue(1)
-    game.setSquareValue(7)
+    setupFinishedGameWithHorizontalLines()
 
     expect(game.playerOne.winner).toBe(true)
   })
 
   it("should set player victory only if game status is finished", () => {
-    const game = new TikTakToe()
     game.setPlayerVictory()
 
     expect(game.playerOne.winner).toBe(false)
@@ -178,7 +198,6 @@ describe("TikTakToe", () => {
   })
 
   it("should not set player victory after theres a winner", () => {
-    const game = new TikTakToe()
     game.status = "finished"
     game.playerOne.winner = true
     game.playerOne.itsTurn = false
@@ -190,46 +209,19 @@ describe("TikTakToe", () => {
   })
 
   it("should check end game returns true if all squares are filled", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(1)
-    game.setSquareValue(2)
-    game.setSquareValue(6)
-    game.setSquareValue(4)
-    game.setSquareValue(5)
-    game.setSquareValue(9)
-    game.setSquareValue(7)
-    game.setSquareValue(8)
+    setupDrawGame()
 
     expect(game.checkEndGame()).toBe(true)
   })
 
   it("should game status be draw if all squares are filled", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(1)
-    game.setSquareValue(2)
-    game.setSquareValue(6)
-    game.setSquareValue(4)
-    game.setSquareValue(5)
-    game.setSquareValue(9)
-    game.setSquareValue(7)
-    game.setSquareValue(8)
+    setupDrawGame()
 
     expect(game.status).toBe("draw")
   })
 
   it("should game status be finished if all squares are filled but with a winner", () => {
-    const game = new TikTakToe()
-    game.setSquareValue(3)
-    game.setSquareValue(1)
-    game.setSquareValue(2)
-    game.setSquareValue(6)
-    game.setSquareValue(4)
-    game.setSquareValue(5)
-    game.setSquareValue(7)
-    game.setSquareValue(9)
-    game.setSquareValue(8)
+    setupFinishedGameWithAllSquaresFilled()
 
     expect(game.status).toBe("finished")
   })
