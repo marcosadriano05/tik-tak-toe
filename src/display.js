f22cd4b5dc2ffcee044f91f15537a65e5492ea00
Marcos Adriano <marcosadriano740@gmail.com>
2022-06-15 21:49:52 -0300

    put all render responsability on its own class

--- src/display.js
+++ src/display.js
@@ -1,9 +1,26 @@
 export class Display {
-  constructor(squares) {
-    this.squares = squares
+  constructor(game, board, message, initButton) {
+    this.game = game
+    this.message = message
+    this.initButton = initButton
+    this.board = board
+
+    this.board.innerHTML = ""
+    this.squares = []
+
+    for (let i = 0; i < 9; i++) {
+      const square = document.createElement("button")
+      square.dataset.square = i + 1
+      square.classList.add("square")
+      this.board.appendChild(square)
+      this.squares.push(square)
+    }
+
+    this.message.innerHTML = "Player: X"
+    this.initButton.innerHTML = "Reboot"
   }
 
-  winColor() {
+  onWin() {
     for (let i = 0; i < 3; i++) {
       if (this.squares[i * 3].innerHTML === this.squares[i * 3 + 1].innerHTML 
         && this.squares[i * 3 + 1].innerHTML === this.squares[i * 3 + 2].innerHTML 
@@ -46,6 +63,27 @@ export class Display {
         this.squares[2].classList.add("win") 
         this.squares[4].classList.add("win")
         this.squares[6].classList.add("win")
+    }
+  }
+
+  render(squareNumber) {
+    if (this.game.status !== "finished") {
+      this.game.setSquareValue(squareNumber)
+      const square = Array.from(this.board.children).find(button => button.dataset.square === `${squareNumber}`)
+      square.innerHTML = this.game.squares.get(squareNumber).value
+      square.classList.add("filled")
+      this.message.innerHTML = this.game.playerOne.itsTurn ? "Player: X" : "Player: O"
+      this.onWin()
+    }
+
+    if (this.game.status === "finished") {
+      this.message.innerHTML = this.game.playerOne.winner ? "Player X win" : "Player O win"
+      this.initButton.innerHTML = "Play again"
+    } 
+    
+    if (this.game.status === "draw") {
+      this.message.innerHTML = "Draw"
+      this.initButton.innerHTML = "Play again"
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -10,18 +10,7 @@ let display
 
 function init() {
   game = new TikTakToe()
-  board.innerHTML = ""
-  let squares = []
-  for (let i = 0; i < 9; i++) {
-    const square = document.createElement("button")
-    square.dataset.square = i + 1
-    square.classList.add("square")
-    board.appendChild(square)
-    squares.push(square)
-  }
-  display = new Display(squares)
-  message.innerHTML = "Player: X"
-  initButton.innerHTML = "Reboot"
+  display = new Display(game, board, message, initButton)
 }
 
 initButton.addEventListener("click", init)
@@ -33,28 +22,5 @@ window.onload = () =>{
 board.addEventListener("pointerdown", (e) => {
   if (e.target.className === "board") return
   const square = Number(e.target.dataset.square)
-  render(square)
+  display.render(square)
 })
-
-function render(squareNumber) {
-  if (game.status !== "finished") {
-    game.setSquareValue(squareNumber)
-    displayInfo()
-    const square = Array.from(board.children).find(button => button.dataset.square === `${squareNumber}`)
-    square.innerHTML = game.squares.get(squareNumber).value
-    square.classList.add("filled")
-    display.winColor()
-  }
-}
-
-function displayInfo() {
-  if (game.status === "finished") {
-    message.innerHTML = game.playerOne.winner ? "Player X win" : "Player O win"
-    initButton.innerHTML = "Play again"
-  } else if (game.status === "draw") {
-    message.innerHTML = "Draw"
-    initButton.innerHTML = "Play again"
-  } else {
-    message.innerHTML = game.playerOne.itsTurn ? "Player: X" : "Player: O"
-  }
-}
