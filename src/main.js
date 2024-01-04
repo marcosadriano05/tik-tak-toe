2a435f94938d0f207943212de11299c01f1f435b
Marcos Adriano <marcosadriano740@gmail.com>
2024-01-03 23:03:26 -0300

    chore: fix node version and update dependencies

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import { Display } from "./display"
 const board = document.querySelector(".board")
 const initButton = document.querySelector("[data-init]")
 const message = document.querySelector("[data-message]")
-let game
+
 let display
 
 function init() {
-  game = new TikTakToe()
+  const game = new TikTakToe()
   display = new Display(game, board, message, initButton)
 }
 
